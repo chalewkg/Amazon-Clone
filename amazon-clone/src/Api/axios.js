efe660afb4a51667020b820caa62a98d659a322a
chalewkg <chalewkg@gmail.com>
2024-02-19 06:47:09 -0500

    created build folder -npm  run build

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
+  // Local instance of firebase
   // baseURL: "http://127.0.0.1:5001/clone-85937/us-central1/api",
+
+  //Deployed version of fire base
   baseURL: "https://api-xyos2dqtva-uc.a.run.app",
+
+  // deployed version of amoazon server on render.com using express with out firebase function
+  // baseURL: "https://amazon-api-deploy-e323.onrender.com/",
 });
 
 export { axiosInstance };
